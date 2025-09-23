7362abe83c4858722406c1433278cf47302beeae
MystCely <157691471+MystCely@users.noreply.github.com>
2025-09-23 20:04:14 +0200

    added css

--- script/view.js
+++ script/view.js
@@ -2,8 +2,10 @@ updateView();
 function updateView() {
   let html = /* html */ `
     <h1>Magic 8 Ball</h1>
-    <div class="circle" onclick="showAdvice()"></div>
-    <div id="result"></div>
+    <div class="center">
+      <div class="circle" onclick="showAdvice()"></div>
+      <div id="result"></div>
+    </div>
     `;
   app.innerHTML = html;
 }
